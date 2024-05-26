632306b9392b63afc22363366dacde12a0237a8d
Pratiksh Kumar <86194461+PratikshKumar@users.noreply.github.com>
2024-05-26 07:20:35 +0000

    maths

---
+++ 01_basics/08_maths.js
@@ -0,0 +1,29 @@
+
+// const balance =156.389;
+// console.log(balance.toString().length);
+// // console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// // fixes the decimal count
+
+// const num=5785.86867
+// console.log(num.toPrecision(5));
+// console.log(num.toPrecision(3));
+
+// const yo=1000000
+// console.log(yo.toLocaleString('en-IN'));
+// console.log(Number.MAX_VALUE);
+
+
+
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.4));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1); // to get num bw 1 and 11(excluding 11)
+console.log((Math.floor(Math.random()*10)+1); // to get num bw 1 to 10(inclusive)
+
+// to get val bw min and max
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
