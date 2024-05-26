3925871cfcbed9f8e4341cee454602666aaf9f21
Pratiksh Kumar <86194461+PratikshKumar@users.noreply.github.com>
2024-05-26 00:01:50 +0000

    file3

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score =33
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let sc2="33abc"
+// console.log(typeof sc2);
+// console.log(typeof (sc2));
+
+// let valInNumber = Number(sc2)
+
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+// console.log("-----------");
+
+// let yo= null;
+// console.log(typeof yo);
+// console.log(yo);
+// console.log(Number(yo));
+
+
+// let yo2= undefined;
+// console.log(typeof yo2);
+// console.log(yo2);
+// console.log(Number(yo2));
+
+// coversion to number: "33"=>33 ; 
+// coversion to number: "33abc" => NaN ; 
+// coversion to number: null => 0 ; 
+// coversion to number: undefined => NaN ;
+
+// console.log("------------");
+
+let flag="";
+let boolFlag= Boolean(flag)
+console.log(boolFlag);
+
+// 1=> true
+// 0=>false
+// "something"=> true
+// ""=> false
+
