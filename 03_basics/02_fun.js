ef89edbf1b5589c5b3f46df5b41fcfbcaa90a76c
Pratiksh Kumar <86194461+PratikshKumar@users.noreply.github.com>
2024-05-27 02:00:57 +0000

    iife

--- 03_basics/02_fun.js
+++ 03_basics/02_fun.js
@@ -8,3 +8,28 @@ fun()
 // we get undefind here becuase this keyword dont work in functions
 
 
+// const lai = function()
+// {
+    
+// }
+// converting abive to arraw wala represengation 
+
+const lai =  () => {
+    let user = "bob";
+    console.log(this);
+}
+
+lai()
+
+
+const add = (n1,n2) =>{
+    return n1+n2;
+}
+
+console.log(add(3,4));
+
+// IMPLICIT RETURN IN A FUNCTION 
+
+const laii= (n1,n2) => (n1+n2)
+console.log(laii(3,5));
+// () ka use hota for returning anyting 
---
+++ 03_basics/03_IIFE.js
@@ -0,0 +1,25 @@
+// IIFE: IMMEDIATLRY INVOKED FN EXPRESSIONS
+
+// we want to immediatley execute a function. 
+// Eg: when we run a file, we want that at tha instant, it should be connected to database. 
+
+// to avoid pulltion ffrom global scope as it can pollute
+//  
+// global scope ke pollution se dikkt hoti kai bar 
+// to uss global scope ke variab;les ke pollution ko htane keliye, we use IIFEE
+
+(function chai()
+{
+    console.log(`DB CONNECTED`);
+})();
+// above is named IIFE
+
+// first () means fn defination and second () means fn execution 
+
+( ()=>{
+    console.log(`DB 2 CONNECTED`);
+} )();
+
+( (name)=>{
+    console.log(`DB 3 coonnetd by ${name}`);
+} )('PATRICK')
