a543571bbd2442a727c2b3585549d2b487667d10
Pratiksh Kumar <86194461+PratikshKumar@users.noreply.github.com>
2024-06-24 14:39:31 +0000

    EC

---
+++ 03_basics/04_Ececution_context.js
@@ -0,0 +1,16 @@
+// Javascipt execution context : JS  file ko run kaise kreggi 
+
+// Gloabl execution context (this) : Browser ka execution context is Window Object . this refers to here as Window Object 
+
+// Functional Execution Context 
+// Eval Execution Context: Property of a global object 
+
+/*
+PHASES OF JS EXECUTION :
+
+1) MEMORY CREATION PHASE 
+2) EXECUTION PHASE 
+
+
+
+*/
